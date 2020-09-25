ea0b0b9f1aff7613045eb1a437186b1a481ab887
i-Tutu <ignatiustutu@gmail.com>
2020-09-25 17:28:09 +0000

    Added: Changes to nav button

--- resource/head-index.php
+++ resource/head-index.php
@@ -1,23 +1,20 @@
 
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-custom fixed-top bg-primary">
-    <div class="container">
+ <nav class="navbar navbar-expand-lg navbar-custom fixed-top bg-primary">
+    <div class="container d-flex justify-content-between-end">
       <a class="navbar-brand text-white" href="index.php">
-      <!-- <img class="img-fluid" src="img/ktu.jpg" alt="ktu"> -->
       Vira Payment</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+     <!--  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-          <!-- <li class="nav-item">
-            <a class="nav-link text-white" href="register.php">Sign Up</a>
-          </li> -->
           <li class="nav-item">
             <a class="nav-link text-white" href="login.php">Log In</a>
           </li>
         </ul>
-      </div>
+      </div> -->
+      <a class="text-white" href="login.php">Log In</a>
     </div>
   </nav>
 
